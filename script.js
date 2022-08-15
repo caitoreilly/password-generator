63d067a10dc892dc351844d65be1983e0b68df95
Caitlin O'Reilly <coreilly29@gmail.com>
2022-08-15 16:58:37 -0400

    generate password function

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn);
+
+// Define constants for letters, numbers, and special characters
+const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = "0123456789";
+const specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +14,57 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
 
+// Prompts that pop up after you click generate password button.
+// User selects and confirms criteria to include in password.
+// Alerts pop up to reiterate what user selected.
+
+function generatePassword() {
+  var confirmLowerCase = window.confirm(
+    "Would you like lowercase letters in your password?"
+  );
+  if (confirmLowerCase) {
+    alert("Your password will have lowercase letters.");
+  } else {
+    alert("Your password will NOT have lowercase letters.");
+  }
+
+  var confirmUpperCase = window.confirm(
+    "Would you like uppercase letters in your password?"
+  );
+  if (confirmUpperCase) {
+    alert("Your password will have uppercase letters.");
+  } else {
+    alert("Your password will NOT have uppercase letters.");
+  }
+
+  var confirmNumbers = window.confirm(
+    "Would you like numbers in your password?"
+  );
+  if (confirmNumbers) {
+    alert("Your password will have numbers.");
+  } else {
+    alert("Your password will NOT have numbers.");
+  }
+
+  var confirmSpecialCharacters = window.confirm(
+    "Would you like special characters in your password?"
+  );
+  if (confirmSpecialCharacters) {
+    alert("Your password will have special characters.");
+  } else {
+    alert("Your password will NOT have special characters.");
+  }
+
+  var promptPasswordLength = window.prompt(
+    "Please select the number of characters you would like in your password. It must be at least 8 characters and no more than 128 characters."
+  );
+  window.alert(
+    "Your password will have a total of " +
+      promptPasswordLength +
+      " characters."
+  );
 }
 
 // Add event listener to generate button
